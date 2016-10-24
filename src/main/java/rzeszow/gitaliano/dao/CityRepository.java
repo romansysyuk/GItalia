6728c7ec1dfaa6389cfc1aa21ac836a3fa8dbf13
romansys <kobebryant41@gmail.com>
2016-10-24 17:13:10 +0200

    test commit

--- src/main/java/rzeszow/gitaliano/dao/CityRepository.java
+++ src/main/java/rzeszow/gitaliano/dao/CityRepository.java
@@ -1,11 +1,20 @@
 package rzeszow.gitaliano.dao;
 
 
+import org.hibernate.annotations.NamedQueries;
+import org.hibernate.annotations.NamedQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import rzeszow.gitaliano.entity.City;
 
 /**
  * Created by kobeb on 23.10.2016.
  */
 public interface CityRepository extends JpaRepository<City,Long> {
+
+    @Query("select c from City c where c = :name ")
+    City findByName(@Param("name") String Name);
+
+
 }
